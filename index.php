d113b05ab071bab0f54635e75cd8507d5dedf98a
Miguel A Aceves <maceves@csumb.edu>
2018-02-03 08:14:25 +0000

    Add all of lab 1 files

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+<!--
+
+First Website
+and comment
+in html
+(comments can span multiple lines)
+
+-->
+
+<!-- This is the head -->
+<!-- All styles and javascript go inside the head -->
+    <head>
+        <meta charset="utf-8" />
+        <title> Miguel Aceves: Personal Website</title>
+        
+        <link href="css/styles.css" rel="stylesheet" type="text/css" />
+    </head>
+<!-- closing head -->
+
+    <!-- This is the body -->
+    <!-- This is where we place the content of our website -->
+    <body>
+        <header>
+            <h1>Miguel Aceves</h1>
+        </header>
+        <nav>
+            <hr width="50%"/>
+            
+            <a href="index.html">Home</a>
+            <a href="about.html">About</a>
+            <a href="contact.html">Contact</a>
+        </nav>
+        
+        <br /> <br />
+        
+        <main>
+            <figure id="me">
+                <img width="275" height="300" src="img/mypicture.jpg" alt="Picture of Miguel Aceves"/>
+            </figure>
+            
+            <div id="welcomeText">
+                Hello <br />
+                <p>Thank your for visisting my professional portfolio website.</p>
+                <p>I am currently a student at California State University Monterey Bay, pursuing a degree in Computer Science. I am also looking for any internship opportunities.</p>
+                <p>Feel free to contact me!</p>
+                <br /><br />
+                
+                <em>"The road to success and the road to failure are almost exactly the same."</em><br />
+                - Colin R. Davis
+                
+            </div>
+            
+        </main>
+        <!-- This is the footer -->
+        <!-- The footer goes inside the body but not always -->
+        <footer>
+            <hr>
+            Internet Programming CST 336. 2017&copy;Miguel Aceves <br />
+            <strong>Disclaimer:</strong> The information in this webpage is fictitous.<br />
+            It is used for academic purposes only.
+            
+            <figure>
+                 <img width="100" height="100" src="img/csumblogo.jpeg" alt="Picture of CSUMB logo"/>
+            </figure>
+        </footer>
+        
+        <!-- closing footer -->
+        
+    </body>
+    <!-- closing body -->
+
+</html>
